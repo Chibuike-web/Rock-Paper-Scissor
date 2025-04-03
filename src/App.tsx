39ad338cb4652a2034753dc95b7b410f8620ba55
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-04-03 04:41:41 +0100

    ui implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import "./globals.css";
 import Triangle from "./assets/bg-triangle.svg";
 import IconPaper from "./assets/icon-paper.svg";
 import IconScissor from "./assets/icon-scissors.svg";
-import IconLizard from "./assets/icon-lizard.svg";
 import IconRock from "./assets/icon-rock.svg";
+import CancelIcon from "./assets/icon-close.svg";
+import Rules from "./assets/image-rules.svg";
+import { useState } from "react";
 
 import styles from "./styles/Paper.module.css";
 
 function App() {
+	const [showModal, setShowModal] = useState(false);
 	return (
-		<main className="pt-12">
-			<section className="mx-auto max-w-[43.75rem] flex w-full items-center justify-between px-5 py-4 border-[3px] border-[var(--headerOutline)] rounded-[1rem] max-h-[9.375rem]">
+		<main className="pt-12 min-h-[768px] ">
+			<section className="mx-auto relative max-w-[43.75rem]  flex w-full items-center justify-between px-5 py-4 border-[3px] border-[var(--headerOutline)] rounded-[1rem] max-h-[9.375rem]">
 				<img src="/logo.svg" alt="Logo" className="w-full max-w-[9.72rem]" />
 				<div className="bg-white justify-items-center py-4 rounded-[0.5rem]">
 					<p className="text-[var(--scoreText)] text-[0.875rem] tracking-wider">SCORE</p>
@@ -18,7 +21,7 @@ function App() {
 				</div>
 			</section>
 
-			<section className="mx-auto relative w-full max-w-[26.75rem] justify-items-center">
+			<section className="mx-auto relative w-full max-w-[26.75rem]  justify-items-center">
 				<img
 					src={Triangle}
 					alt="Triangle background"
@@ -49,8 +52,48 @@ function App() {
 					</figure>
 				</button>
 			</section>
+			<p
+				className="text-white absolute right-[2rem] bottom-[2rem] border-[1px] px-6 py-[8px] rounded-[8px] tracking-[0.2em] cursor-pointer"
+				onClick={() => setShowModal(true)}
+			>
+				RULES
+			</p>
+			{showModal && <RulesModal setShowModal={setShowModal} />}
 		</main>
 	);
 }
+
+const RulesModal = ({ setShowModal }: { setShowModal: (value: boolean) => void }) => {
+	return (
+		<div
+			className="bg-black/50 fixed inset-0 justify-items-center content-center"
+			onClick={(e) => {
+				e.stopPropagation();
+				setShowModal(false);
+			}}
+		>
+			<article
+				className="bg-white px-8 py-8 w-full max-w-[400px] rounded-xl justify-items-center"
+				onClick={(e) => {
+					e.stopPropagation();
+				}}
+			>
+				<header className="flex justify-between mb-10 w-full items-center">
+					<h1 className="text-[32px] font-bold">RULES</h1>
+					<button
+						className="cursor-pointer"
+						type="button"
+						onClick={() => {
+							setShowModal(false);
+						}}
+					>
+						<img src={CancelIcon} alt="Cancel Icon" />
+					</button>
+				</header>
+				<img src={Rules} alt="image of the rules" />
+			</article>
+		</div>
+	);
+};
 
 export default App;
