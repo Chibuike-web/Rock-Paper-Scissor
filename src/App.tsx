1d5b599b4d393c92daff03792a5b48349825888d
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-04-04 10:35:58 +0100

    implemented the game functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./components/Modal";
 import PlaySection from "./components/PlaySection";
 import GameSection from "./components/GameSection";
@@ -8,27 +8,81 @@ function App() {
 	const [play, setPlay] = useState<boolean>(false);
 	const [playerId, setPlayerId] = useState<string>("");
 	const [computerId, setComputerId] = useState<string>("");
+	const [result, setResult] = useState<string>("");
+	const [points, setPoints] = useState<number>(0);
+
+	useEffect(() => {
+		if (!playerId || !computerId) return;
+
+		const timer = setTimeout(() => {
+			let newResult = "";
+			if (playerId === computerId) {
+				newResult = "DRAW";
+			} else if (
+				(playerId === "rock" && computerId === "scissor") ||
+				(playerId === "scissor" && computerId === "paper") ||
+				(playerId === "paper" && computerId === "rock")
+			) {
+				newResult = "YOU WIN";
+			} else {
+				newResult = "YOU LOSE";
+			}
+
+			setResult(newResult);
+			if (newResult === "YOU WIN") {
+				setPoints((prev) => +prev + 1);
+			} else if (newResult === "YOU LOSE") {
+				setPoints((prev) => prev - 1);
+			} else {
+				setPoints((prev) => prev);
+			}
+		}, 1000);
+
+		return () => clearTimeout(timer);
+	}, [playerId, computerId]);
+
+	const computerPlayer = () => {
+		setTimeout(() => {
+			setComputerId("rock");
+		}, 1000);
+	};
 
 	const handleClick = (id: string) => {
 		console.log(id);
 		setPlay(true);
 		setPlayerId(id);
-		setComputerId("rock");
+		computerPlayer();
+	};
+
+	const playAgain = () => {
+		setPlay(false);
+		setPlayerId("");
+		setComputerId("");
+		setResult("");
 	};
 	return (
 		<main className="pt-12">
 			<div className="w-full min-h-full">
 				<section className="mx-auto relative max-w-[43.75rem] flex w-full items-center justify-between px-5 py-4 border-[3px] border-[var(--headerOutline)] rounded-[1rem] max-h-[9.375rem]">
 					<img src="/logo.svg" alt="Logo" className="w-full max-w-[9.72rem]" />
-					<div className="bg-white justify-items-center py-4 rounded-[0.5rem]">
+					<div className="bg-white justify-items-center py-4 rounded-[0.5rem] w-full max-w-[120px]">
 						<p className="text-[var(--scoreText)] text-[0.875rem] tracking-wider">SCORE</p>
-						<h1 className="text-[3rem] font-bold text-[var(--darkText)] px-10 leading-[1em]">12</h1>
+						<h1 className="text-[3rem] font-bold text-[var(--darkText)] px-10 leading-[1em]">
+							{points}
+						</h1>
 					</div>
 				</section>
 
 				{!play && <PlaySection handleClick={handleClick} />}
 
-				{play && <GameSection playerId={playerId} computerId={computerId} />}
+				{play && (
+					<GameSection
+						playerId={playerId}
+						computerId={computerId}
+						result={result}
+						playAgain={playAgain}
+					/>
+				)}
 				<Modal />
 			</div>
 		</main>
--- src/components/GameSection.tsx
+++ src/components/GameSection.tsx
@@ -1,31 +1,40 @@
 import GameButton from "../components/GameButton";
 
-export default function GameSection({
-	playerId,
-	computerId,
-}: {
+type GameSectionProps = {
 	playerId: string;
 	computerId: string;
-}) {
+	result: string;
+	playAgain: () => void;
+};
+
+export default function GameSection({ playerId, computerId, result, playAgain }: GameSectionProps) {
 	return (
-		<section className="justify-items-center py-[88px]">
+		<section className="justify-items-center py-[104px]">
 			<div className="text-white flex items-center gap-[76px]">
-				<div className="flex flex-col items-center gap-[64px]">
-					<p className="tracking-[0.2em] text-[20px]">YOU PICKED</p>
+				<div className="flex flex-col items-center gap-[64px] relative">
+					<p className="tracking-[0.2em] text-[20px] absolute top-[-30%]">YOU PICKED</p>
 					<GameButton buttonId={playerId} />
 				</div>
-				<div className="justify-items-center">
-					<h1 className="text-[48px] mb-[8px]">YOU WIN</h1>
-					<button
-						type="button"
-						className="text-[var(--darkText)] bg-white tracking-[0.2em] px-10 py-3 rounded-[6px] cursor-pointer hover:text-[hsl(349,71%,52%)]"
-					>
-						PLAY AGAIN
-					</button>
-				</div>
-				<div className="flex flex-col items-center gap-[64px]">
-					<p className="tracking-[0.2em] text-[20px]">YOU PICKED</p>
-					<GameButton buttonId={computerId} />
+				{result && (
+					<div className="justify-items-center">
+						<h1 className="text-[48px] mb-[8px]">{result}</h1>
+						<button
+							type="button"
+							className="text-[var(--darkText)] bg-white tracking-[0.2em] px-10 py-3 rounded-[6px] cursor-pointer hover:text-[hsl(349,71%,52%)]"
+							onClick={() => playAgain()}
+						>
+							PLAY AGAIN
+						</button>
+					</div>
+				)}
+
+				<div className="flex flex-col items-center gap-[64px] relative">
+					<p className="tracking-[0.2em] text-[20px] absolute top-[-30%]">THE HOUSE PICKED</p>
+					{computerId ? (
+						<GameButton buttonId={computerId} />
+					) : (
+						<div className="w-[16rem] h-[16rem] bg-black/10 rounded-full"></div>
+					)}
 				</div>
 			</div>
 		</section>
